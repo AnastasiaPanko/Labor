baf9fd068a2839c9228f29aa59dd39f9725d9601
AnastasiaPanko <anastasiaa.pankoo@gmail.com>
2021-06-10 10:03:49 +0300

    Recreate RyCrossroads

--- PyCrossroads/main.py
+++ PyCrossroads/main.py
@@ -1,6 +1,30 @@
 timeDoingFirstGreenLight = int(input("Введіть час горіння першого зеленого світла на світлофорі:"))
 timeDoingSecondGreenLight = int(input("Введіть час горіння другого зеленого світла на світлофорі:"))
 roadWidth = 4
-numberOfLineAtFirstRoad = int(input("Введіть кількість ліній на першій дорозі:"))
-numberOfLineAtSecondRoad = int(input("Введіть кількість ліній на другій дорозі:"))
+numberOfLineAtFirstRoad = int(input("Введіть кількість ліній на першій дорозі(1-2):"))
+numberOfLineAtSecondRoad = int(input("Введіть кількість ліній на другій дорозі(1-2):"))
 numberOfCar = ((timeDoingFirstGreenLight*10*roadWidth*numberOfLineAtSecondRoad)/100)+((timeDoingSecondGreenLight*10*roadWidth*numberOfLineAtFirstRoad)/100)
+if(numberOfLineAtFirstRoad == 1):
+    print("      ║  ^  ║")
+    print("      ║  ^  ║")
+    print("══════╝  ^  ╚══════")
+    if(numberOfLineAtSecondRoad == 2):
+        print(" > > >       > > >")
+    if(numberOfLineAtSecondRoad == 2):
+        print(" > > >       > > >")
+        print(" > > >       > > >")
+    print("══════╗  ^  ╔══════")
+    print('      ║  ^  ║')
+    print('      ║  ^  ║')
+if(numberOfLineAtFirstRoad == 2):
+    print("      ║  ^ ^  ║")
+    print("      ║  ^ ^  ║")
+    print("══════╝  ^ ^  ╚══════")
+    if(numberOfLineAtSecondRoad == 1):
+        print(" > > >         > > >")
+    if(numberOfLineAtSecondRoad == 2):
+        print(" > > >         > > >")
+        print(" > > >         > > >")
+    print("══════╗  ^ ^  ╔══════")
+    print('      ║  ^ ^  ║')
+    print('      ║  ^ ^  ║')
