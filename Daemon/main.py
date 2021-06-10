a4a078ab0191245ef6a6640073b0fd382a303ff9
AnastasiaPanko <anastasiaa.pankoo@gmail.com>
2021-06-10 15:12:38 +0300

    Daemon

---
+++ Daemon/main.py
@@ -0,0 +1,18 @@
+import ctypes
+from infi.systray import SysTrayIcon
+
+
+def turnOffKeyboard(systray):
+    ctypes.windll.user32.BlockInput(True)
+
+
+def turnOnKeyboard(systray):
+    ctypes.windll.user32.BlockInput(False)
+
+
+menu_options = (
+    ("turn Off Keyboard", None, turnOffKeyboard),
+    ("turn on Keyboard", None, turnOnKeyboard),
+)
+systray = SysTrayIcon("D://image.ico", "Example tray icon", menu_options)
+systray.start()
